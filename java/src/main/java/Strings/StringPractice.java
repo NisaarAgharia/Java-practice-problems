e12a398414560f82724221d6ba76d33d662cb395
Nisaar <nagharia786@gmail.com>
2021-08-30 15:58:55 +0530

    adding String

---
+++ java/src/main/java/Strings/StringPractice.java
@@ -0,0 +1,32 @@
+package Strings;
+
+public class StringPractice {
+
+    public static void main(String[] args) {
+
+        String s1 = "Nisaar";
+        String s2 = " Agharia";
+        String s3 = "";
+//never do this - string manipulation with String in loops use string buffer or builder instead.
+       /* for (int i=0 ; i< 100000; i++)
+        {
+            s3= s1+s2 +s3;
+            System.out.println(s3);
+        }*/
+
+
+        StringBuilder sb = new StringBuilder(s2);
+        StringBuilder sba = new StringBuilder("3.3airawg");
+        sb.replace(0,2,"bz");
+        sb.insert(3,"w");
+        sb.append(3.3f);
+        sb.delete(4,5);
+        sb.reverse();
+
+
+        System.out.println( sb.compareTo(sba));
+        System.out.println(sb);
+
+
+    }
+}
