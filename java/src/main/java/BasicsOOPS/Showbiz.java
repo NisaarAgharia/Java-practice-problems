67b65dbb0d0e4b22b1b8871d5fc8d1bda5565727
Nisaar <nagharia786@gmail.com>
2021-09-02 20:22:41 +0530

    adding java files

---
+++ java/src/main/java/BasicsOOPS/Showbiz.java
@@ -0,0 +1,11 @@
+package BasicsOOPS;
+
+public interface Showbiz {
+    default void showsha(){
+        System.out.println("i am default implementation");
+    }
+
+    public void business();
+
+
+}
