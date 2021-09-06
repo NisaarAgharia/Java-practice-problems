54d4bf51fc73bba8590a52fec167338f3a8cdfd4
Nisaar <nagharia786@gmail.com>
2021-09-06 17:52:08 +0530

    ExecutorService

---
+++ java/src/main/java/multiThreading/practice/ScoreExecutor.java
@@ -0,0 +1,13 @@
+package multiThreading.practice;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ScoreExecutor {
+
+    public static void main(String[] args) {
+        ExecutorService executorService1 = Executors.newFixedThreadPool(10);
+
+       // executorService1.submit();
+    }
+}
