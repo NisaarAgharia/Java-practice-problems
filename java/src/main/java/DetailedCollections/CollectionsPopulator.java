c37cdeeb452edf2ae850c4b4fafe8b4bacde1890
Nisaar <nagharia786@gmail.com>
2021-10-04 19:09:35 +0530

    Adding Collections Populator Class file

---
+++ java/src/main/java/DetailedCollections/CollectionsPopulator.java
@@ -0,0 +1,18 @@
+package DetailedCollections;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CollectionsPopulator {
+    private Employee emp;
+    private Collections col;
+
+    public CollectionsPopulator(Employee emp, Collections col) {
+        this.emp = emp;
+        this.col = col;
+    }
+
+    public void populate(Collection c , Employee e){
+
+    }
+}
