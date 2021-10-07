72b2ce5d96e5b8adfd5be261bbe7d3af7162d12e
Nisaar <nagharia786@gmail.com>
2021-10-07 18:51:40 +0530

    Hash Map Collection

--- java/src/main/java/DetailedCollections/HashMapCollection.java
+++ java/src/main/java/DetailedCollections/HashMapCollection.java
@@ -1,8 +1,32 @@
 package DetailedCollections;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class HashMapCollection {
 
     public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+map.put(1,"One");
+map.put(2,"Two");
+
+        Set s = map.entrySet();
+        System.out.println(s);
+        for(Map.Entry m : map.entrySet())
+        {
+            System.out.println(m.getKey());
+            System.out.println(m.getValue());
+        }
+
+        while(!map.isEmpty())
+        {
+            map.remove(1);
+            map.replace(2,"three");
+            map.remove(2);
+
+        }
+        System.out.println(map);
 
     }
 }
