c4efcec5bc4289c0d95d399e8cb3fb49d3801842
Nisaar <nagharia786@gmail.com>
2021-10-10 14:03:23 +0530

    Double Ended QUEUE

--- java/src/main/java/DetailedCollections/queues/DQ.java
+++ java/src/main/java/DetailedCollections/queues/DQ.java
@@ -1,11 +1,16 @@
 package DetailedCollections.queues;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class DQ {
     public static void main(String[] args) {
         Deque dq  = new LinkedList();
+        ArrayDeque ad = new ArrayDeque();
+
+        Queue q = new LinkedList();
 
     }
 }
